63d0d6ab9cd1268fdcd26ff4f931bc2ca2f3b2be
buchanKim <kbugang0321@gmail.com>
2025-03-06 11:06:16 +0900

    fist commit

---
+++ lib/main.dart
@@ -0,0 +1,14 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class myApp extends StatelessWidget {
+  const myApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const MaterialApp();
+  }
+}
